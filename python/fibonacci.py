de4297f2f43f27f69e76d697a59772fab8c62301
Daniel Tayal <tayaldaniel2@gmail.com>
2024-10-22 17:24:25 +0530

    Update fibonacci.py

--- python/fibonacci.py
+++ python/fibonacci.py
@@ -1,3 +1,17 @@
 def fibonacci(n):
     """Generates a list of the first n Fibonacci numbers."""
-    pass  # TODO: Implement this function
+    if type(n) != int or n <= 0: # if datatype of n not a integer or n less than equal to 0
+        return "Input must be a positive integer."
+
+    fib_sequence = [] # empty list
+
+    a, b = 0, 1 # 2 fibpnacci numbers
+
+    for x in range(n): # loop
+        fib_sequence.append(a)  # Add the current number to the list
+        a, b = b, a + b  # Move to the next Fibonacci numbers
+    
+    return fib_sequence
+
+n = int(input()) 
+print(fibonacci(n))
