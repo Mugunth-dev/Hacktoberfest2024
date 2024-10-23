5ed43ea09ec6aa53bcfb0f0f6cf48b80b31b8898
bghk9495@gmail.com <harekrishnabagh14@gmail.com>
2024-10-23 19:15:22 +0530

    Modified function ReverseWords.py

--- python/ReverseWords.py
+++ python/ReverseWords.py
@@ -1,4 +1,13 @@
 #Reverse Words in a Sentence
 def reverse_words(sentence):
     """Reverses the order of words in the sentence."""
-    pass  # TODO: Implement this function
+    # Step 1: Split the sentence into words
+    words = sentence.split()
+
+    # Step 2: Reverse the list of words
+    reversed_words = words[::-1]
+
+    # Step 3: Join the reversed words into a sentence
+    reversed_sentence = ' '.join(reversed_words)
+
+    return reversed_sentence
