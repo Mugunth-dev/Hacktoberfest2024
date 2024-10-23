dfb69ff8519608fa9cdc41a40d875145f4d90c4f
Unsigned <72160182+shouryaAr@users.noreply.github.com>
2024-10-23 17:04:18 +0530

    ReverseWords.py
    Added the program to reverse words in a sentence

--- python/ReverseWords.py
+++ python/ReverseWords.py
@@ -1,4 +1,11 @@
 #Reverse Words in a Sentence
 def reverse_words(sentence):
-    """Reverses the order of words in the sentence."""
-    pass  # TODO: Implement this function
+    """ Split the input string by ' ' while 
+    ignoring multiple consecutive spaces """
+    words = [word for word in sentence.split(' ') if word]
+    
+    # Reverse the words
+    words.reverse()
+    
+    # Join the reversed words back into a string
+    return ' '.join(words)
